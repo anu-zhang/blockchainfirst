0fd3411133da7f161e741f5b2fda7670cb32faf2
anu-zhang <michael_u@163.com>
2025-01-21 15:58:58 +0800

    init

---
+++ Counter.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+contract Counter {
+
+    uint256  value;
+    function initiallize(uint x)public  { 
+       value = x;
+        }
+
+        function get() view public returns (uint){
+            return value;
+        }
+
+        function increment (uint n) public {
+            value = value +n;
+        }
+        function decrement(uint n) public{  //decrements and returns the new value
+             
+                value= value -n;
+ 
+        }
+}
+
+
